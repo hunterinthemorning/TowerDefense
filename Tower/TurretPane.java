b2a7708d914b1e0305c24d53d02cdf469324e5c8
Bryant Saenz <bryantsaenz@gmail.com>
2013-04-13 17:25:45 -0400

    Changes Josh did for stats

--- Tower/TurretPane.java
+++ Tower/TurretPane.java
@@ -9,11 +9,12 @@ public class TurretPane extends JPanel{
 		String level = Integer.toString(Data.getLevel());
 		String gold = Integer.toString(Data.getGold());
 		String life = Integer.toString(Data.getLife());
-		g.drawString("Level: ", 5, 10);
-		g.drawString(level, 5, 15);
-		g.drawString("Gold: ", 10, 10);
-		g.drawString(gold, 10, 15);
-		g.drawString("Life: ", 10, 20);
-		g.drawString(life, 10, 25);
+		g.drawString("Level: ", 10, 10);
+		g.drawString(level, 10, 30);
+		g.drawString("Gold: ", 10, 50);
+		g.drawString(gold, 10, 70);
+		g.drawString("Life: ", 10, 90);
+		g.drawString(life, 10, 110);
+
 	}
 }
